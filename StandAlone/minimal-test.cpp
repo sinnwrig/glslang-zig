3be328b820917b66217678dbf8fa7adf681ad878
Kai Angulo <kangriebling@gmail.com>
2024-10-23 23:24:05 -0700

    feat: add process logs to minimal test

--- StandAlone/minimal-test.cpp
+++ StandAlone/minimal-test.cpp
@@ -5,7 +5,11 @@
 
 int main(void)
 {
+    std::cout << "Initialize process" << std::endl;
+
     glslang_initialize_process();
+
+    std::cout << "Initialized process" << std::endl;
 
     glslang_stage_t stage = GLSLANG_STAGE_FRAGMENT;
 
@@ -74,7 +78,11 @@ float4 pixel(VertexOutput input) : SV_Target
         .resource = glslang_default_resource(),
     };
 
+    std::cout << "Setup default resource" << std::endl;
+
     glslang_shader_t* shader = glslang_shader_create(&input);
+
+    std::cout << "Created shader" << std::endl;
 
     if (!glslang_shader_preprocess(shader, &input))	{
         printf("HLSL preprocessing failed %s\n", fileName);
@@ -84,6 +92,8 @@ float4 pixel(VertexOutput input) : SV_Target
         glslang_shader_delete(shader);
         return 1;
     }
+
+    std::cout << "Preprocessed shader" << std::endl;
 
     if (!glslang_shader_parse(shader, &input)) {
         printf("HLSL parsing failed %s\n", fileName);
@@ -95,8 +105,15 @@ float4 pixel(VertexOutput input) : SV_Target
         return 1;
     }
 
+    std::cout << "Parsed shader" << std::endl;
+
     glslang_program_t* program = glslang_program_create();
+
+    std::cout << "Created program" << std::endl;
+
     glslang_program_add_shader(program, shader);
+
+    std::cout << "Add shader to program" << std::endl;
 
     if (!glslang_program_link(program, GLSLANG_MSG_SPV_RULES_BIT | GLSLANG_MSG_VULKAN_RULES_BIT)) {
         printf("HLSL linking failed %s\n", fileName);
@@ -108,20 +125,32 @@ float4 pixel(VertexOutput input) : SV_Target
         return 1;
     }
 
+    std::cout << "Linked program" << std::endl;
+
     glslang_program_SPIRV_generate(program, stage);
+    
+    std::cout << "Generate SPIRV" << std::endl;
 
     size_t size = glslang_program_SPIRV_get_size(program);
     uint32_t* words = static_cast<uint32_t*>(malloc(size * sizeof(uint32_t)));
     glslang_program_SPIRV_get(program, words);
 
+    std::cout << "Got SPIRV" << std::endl;
+
     const char* spirv_messages = glslang_program_SPIRV_get_messages(program);
     if (spirv_messages)
         printf("(%s) %s\b", fileName, spirv_messages);
 
+    std::cout << "Got messages" << std::endl;
+
     glslang_program_delete(program);
     glslang_shader_delete(shader);
 
+    std::cout << "Free program and shader" << std::endl;
+
     char* disassembled = glslang_SPIRV_disassemble(words, size);
+
+    std::cout << "Disassemble SPIRV" << std::endl;
 
     std::cout << "Generated " << size << " SPIR-V words" << std::endl;
     std::cout << disassembled << std::endl;
